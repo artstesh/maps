cdf628430c3bd75ed977bde72bf983b23b2460b1
artstesh <artstesh@mail.ru>
2023-12-13 19:14:13 +0300

    4.0.2

--- projects/maps-components/src/map/messages/commands/remove-tile.command.ts
+++ projects/maps-components/src/map/messages/commands/remove-tile.command.ts
@@ -1,7 +1,7 @@
-import { GenericMessage } from "../generic-message";
-import BaseLayer from "ol/layer/Base";
-import TileLayer from "ol/layer/Tile";
-import { XYZ } from "ol/source";
+import { GenericMessage } from '../generic-message';
+import BaseLayer from 'ol/layer/Base';
+import TileLayer from 'ol/layer/Tile';
+import { XYZ } from 'ol/source';
 
 export class RemoveTileCommand extends GenericMessage {
   public static readonly ID = 'c36ccc0b-5638-4819-b148-2de57a59a5b7';
