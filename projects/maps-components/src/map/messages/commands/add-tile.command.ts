a229d6bf84c581ad926904bb327750aa93bd3f9b
artstesh <artstesh@mail.ru>
2023-12-13 19:14:20 +0300

    5.0.2

--- projects/maps-components/src/map/messages/commands/add-tile.command.ts
+++ projects/maps-components/src/map/messages/commands/add-tile.command.ts
@@ -1,8 +1,8 @@
-import { GenericMessage } from "../generic-message";
-import { Vector as Layer } from "ol/layer";
-import VectorSource from "ol/source/Vector";
-import TileLayer from "ol/layer/Tile";
-import { XYZ } from "ol/source";
+import { GenericMessage } from '../generic-message';
+import { Vector as Layer } from 'ol/layer';
+import VectorSource from 'ol/source/Vector';
+import TileLayer from 'ol/layer/Tile';
+import { XYZ } from 'ol/source';
 
 export class AddTileCommand extends GenericMessage {
   public static readonly ID = '25b7df28-72ce-4453-9fe6-54285a2fdc57';
@@ -15,4 +15,3 @@ export class AddTileCommand extends GenericMessage {
     return AddTileCommand.ID;
   }
 }
-
