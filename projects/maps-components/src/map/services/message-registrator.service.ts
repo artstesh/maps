fed5a123ac0b53c0050cf3c7d15af1d827090d72
artstesh <artstesh@mail.ru>
2023-12-16 15:24:46 +0300

    5.0.3

--- projects/maps-components/src/map/services/message-registrator.service.ts
+++ projects/maps-components/src/map/services/message-registrator.service.ts
@@ -7,19 +7,22 @@ import { PlaceLayerFeaturesCommand } from '../messages/commands/place-layer-feat
 import { RemoveLayerCommand } from '../messages/commands/remove-layer.command';
 import { AddTileCommand } from '../messages/commands/add-tile.command';
 import { RemoveTileCommand } from '../messages/commands/remove-tile.command';
-import { PostboyAbstractRegistrator } from "@artstesh/postboy";
-import { MapManagementService } from "./map-management.service";
-import { MapStateService } from "./map-state.service";
-import { MapClickService } from "./map-click.service";
-import { MapClickEvent } from "../messages/events/map-click.event";
+import { PostboyAbstractRegistrator } from '@artstesh/postboy';
+import { MapManagementService } from './map-management.service';
+import { MapStateService } from './map-state.service';
+import { MapClickService } from './map-click.service';
+import { MapClickEvent } from '../messages/events/map-click.event';
 
 @Injectable()
 export class MessageRegistratorService extends PostboyAbstractRegistrator {
-  constructor(service: MapPostboyService, management: MapManagementService,
-              state: MapStateService,
-              interaction: MapClickService) {
+  constructor(
+    service: MapPostboyService,
+    management: MapManagementService,
+    state: MapStateService,
+    interaction: MapClickService,
+  ) {
     super(service);
-    this.registerServices([management, state,interaction])
+    this.registerServices([management, state, interaction]);
   }
   protected _up(): void {
     this.registerReplay<MapRenderedEvent>(MapRenderedEvent.ID);
