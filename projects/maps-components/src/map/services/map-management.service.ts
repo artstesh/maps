4aad37cf6415071e4eb3520f385d41b97596842c
artstesh <artstesh@mail.ru>
2023-12-16 15:24:21 +0300

    4.0.3

--- projects/maps-components/src/map/services/map-management.service.ts
+++ projects/maps-components/src/map/services/map-management.service.ts
@@ -9,15 +9,14 @@ import { MapRenderedEvent } from '../messages';
 import { PlaceLayerFeaturesCommand } from '../messages/commands/place-layer-features.command';
 import { AddTileCommand } from '../messages/commands/add-tile.command';
 import { RemoveTileCommand } from '../messages/commands/remove-tile.command';
-import { IPostboyDependingService } from "@artstesh/postboy";
+import { IPostboyDependingService } from '@artstesh/postboy';
 
 @Injectable()
 export class MapManagementService implements IPostboyDependingService {
   private map?: Map;
   private layers: { [name: string]: Layer<VectorSource<any>> } = {};
 
-  constructor(private postboy: MapPostboyService) {
-  }
+  constructor(private postboy: MapPostboyService) {}
 
   up(): void {
     this.observeMapRender();
