a01dc03618f50e9af24f722739fea4cbd74ccea3
artstesh <artstesh@mail.ru>
2023-12-16 15:24:05 +0300

    1.0.3

--- projects/maps-components/src/map/services/map-postboy.service.ts
+++ projects/maps-components/src/map/services/map-postboy.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { PostboyService } from '@artstesh/postboy';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MapPostboyService extends PostboyService {
-
   public observe = <T>(id: string): Observable<T> => this.subscribe<T>(id);
 }
--- projects/maps-components/src/map/services/map-state.service.ts
+++ projects/maps-components/src/map/services/map-state.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { Map } from 'ol';
 import { MapRenderedEvent } from '../messages';
 import { MapPostboyService } from './map-postboy.service';
-import { IPostboyDependingService } from "@artstesh/postboy";
+import { IPostboyDependingService } from '@artstesh/postboy';
 
 @Injectable()
 export class MapStateService implements IPostboyDependingService {
   private map?: Map;
 
-  constructor(private postboy: MapPostboyService) {
-  }
+  constructor(private postboy: MapPostboyService) {}
 
   up(): void {
     this.postboy.subscribe<Map>(MapRenderedEvent.ID).subscribe((m) => (this.map = m));
