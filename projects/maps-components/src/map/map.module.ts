e00ce40e2fc2e85572a60d64767c7bc1b4b87b5b
artstesh <artstesh@mail.ru>
2023-12-20 22:08:21 +0300

    2.0.6

--- projects/maps-components/src/map/map.module.ts
+++ projects/maps-components/src/map/map.module.ts
@@ -7,7 +7,7 @@ import { OsmTileLayerComponent } from './map-plate/layers/osm-tile-layer/osm-til
 import { FeatureLayerComponent } from './map-plate/layers/feature-layer/feature-layer.component';
 import { MarkersComponent } from './map-plate/features/markers/markers.component';
 import { TooltipComponent } from './map-plate/tooltips/tooltip/tooltip.component';
-import { PolygonsComponent } from "./map-plate/features/polygons/polygons.component";
+import { PolygonsComponent } from './map-plate/features/polygons/polygons.component';
 
 @NgModule({
   imports: [CommonModule],
@@ -19,8 +19,15 @@ import { PolygonsComponent } from "./map-plate/features/polygons/polygons.compon
     FeatureLayerComponent,
     MarkersComponent,
     TooltipComponent,
-    PolygonsComponent
+    PolygonsComponent,
   ],
-  exports: [MapPlateComponent, FeatureLayerComponent, MarkersComponent,TileLayerComponent,TooltipComponent,PolygonsComponent],
+  exports: [
+    MapPlateComponent,
+    FeatureLayerComponent,
+    MarkersComponent,
+    TileLayerComponent,
+    TooltipComponent,
+    PolygonsComponent,
+  ],
 })
 export class MapModule {}
