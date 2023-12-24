28085aa42ca70cf451c22ac34be5e972951259f9
artstesh <artstesh@mail.ru>
2023-12-24 18:24:02 +0300

    5.0.7

--- projects/maps-components/src/map/map-plate/layers/index.ts
+++ projects/maps-components/src/map/map-plate/layers/index.ts
@@ -3,5 +3,5 @@ export * from './tile-layer/tile-layer.settings';
 export * from './osm-tile-layer/osm-tile-layer.component';
 export * from './feature-layer/feature-layer.settings';
 export * from './feature-layer/feature-layer.component';
-export * from './cluster-layer/cluster-layer.component'
-export * from './cluster-layer/cluster-layer.settings'
+export * from './cluster-layer/cluster-layer.component';
+export * from './cluster-layer/cluster-layer.settings';
--- projects/maps-components/src/map/services/message-registrator.service.ts
+++ projects/maps-components/src/map/services/message-registrator.service.ts
@@ -7,22 +7,26 @@ import { PlaceLayerFeaturesCommand } from '../messages/commands/place-layer-feat
 import { RemoveLayerCommand } from '../messages/commands/remove-layer.command';
 import { AddTileCommand } from '../messages/commands/add-tile.command';
 import { RemoveTileCommand } from '../messages/commands/remove-tile.command';
-import { PostboyAbstractRegistrator } from "@artstesh/postboy";
-import { MapManagementService } from "./map-management.service";
-import { MapStateService } from "./map-state.service";
-import { MapClickService } from "./map-click.service";
-import { MapClickEvent } from "../messages/events/map-click.event";
-import { CloseTooltipCommand } from "../messages/commands/close-tooltip.command";
-import { MapFeatureService } from "./map-feature.service";
-import { FitToFeaturesCommand } from "../messages/commands/fit-to-features.command";
+import { PostboyAbstractRegistrator } from '@artstesh/postboy';
+import { MapManagementService } from './map-management.service';
+import { MapStateService } from './map-state.service';
+import { MapClickService } from './map-click.service';
+import { MapClickEvent } from '../messages/events/map-click.event';
+import { CloseTooltipCommand } from '../messages/commands/close-tooltip.command';
+import { MapFeatureService } from './map-feature.service';
+import { FitToFeaturesCommand } from '../messages/commands/fit-to-features.command';
 
 @Injectable()
 export class MessageRegistratorService extends PostboyAbstractRegistrator {
-  constructor(service: MapPostboyService, management: MapManagementService,
-              state: MapStateService, feature: MapFeatureService,
-              interaction: MapClickService) {
+  constructor(
+    service: MapPostboyService,
+    management: MapManagementService,
+    state: MapStateService,
+    feature: MapFeatureService,
+    interaction: MapClickService,
+  ) {
     super(service);
-    this.registerServices([management, state,interaction,feature])
+    this.registerServices([management, state, interaction, feature]);
   }
   protected _up(): void {
     this.registerReplay<MapRenderedEvent>(MapRenderedEvent.ID);
