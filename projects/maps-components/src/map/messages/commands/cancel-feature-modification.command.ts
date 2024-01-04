0ae8667139f9439bf24723b79114e7ccf5d4b778
artstesh <artstesh@mail.ru>
2024-01-04 12:44:42 +0300

    4.0.11

--- projects/maps-components/src/map/messages/commands/cancel-feature-modification.command.ts
+++ projects/maps-components/src/map/messages/commands/cancel-feature-modification.command.ts
@@ -1,4 +1,4 @@
-import { PostboyGenericMessage } from "@artstesh/postboy";
+import { PostboyGenericMessage } from '@artstesh/postboy';
 
 export class CancelFeatureModificationCommand extends PostboyGenericMessage {
   public static readonly ID: string = '9c9fb40f-ea95-4704-8177-0e9ecabf36fa';
