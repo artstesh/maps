99c16e306f2794e3b4fda4c38ff3f34f3df03b3f
artstesh <artstesh@mail.ru>
2024-02-18 15:09:50 +0300

    5.1.5

--- projects/maps-components/src/map/messages/executors/calculate-area.executor.ts
+++ projects/maps-components/src/map/messages/executors/calculate-area.executor.ts
@@ -1,7 +1,7 @@
-import { PostboyExecutor } from "@artstesh/postboy";
-import { Feature } from "ol";
-import { Geometry } from "ol/geom";
-import { PolygonModel } from "../../models";
+import { PostboyExecutor } from '@artstesh/postboy';
+import { Feature } from 'ol';
+import { Geometry } from 'ol/geom';
+import { PolygonModel } from '../../models';
 
 export class CalculateAreaExecutor extends PostboyExecutor<Feature<Geometry>[]> {
   public static readonly ID: string = '172ecf46-86b5-4f46-a5f1-96ba2cbb1694';
