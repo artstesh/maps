186d4945897e69f7059bb579c7991e607a09eea0
artstesh <artstesh@mail.ru>
2024-03-15 11:44:49 +0300

    3.1.7

--- projects/maps-components/src/map/messages/commands/fit-to-polygons.command.ts
+++ projects/maps-components/src/map/messages/commands/fit-to-polygons.command.ts
@@ -1,7 +1,7 @@
-import { PostboyGenericMessage } from "@artstesh/postboy";
-import { Feature } from "ol";
-import { Geometry } from "ol/geom";
-import { PolygonModel } from "../../models";
+import { PostboyGenericMessage } from '@artstesh/postboy';
+import { Feature } from 'ol';
+import { Geometry } from 'ol/geom';
+import { PolygonModel } from '../../models';
 
 export class FitToPolygonsCommand extends PostboyGenericMessage {
   public static readonly ID: string = 'a9e42b23-ffaf-4c46-8370-e0432cd14d60';
--- projects/maps-components/src/map/messages/queries/get-features-in-area.query.ts
+++ projects/maps-components/src/map/messages/queries/get-features-in-area.query.ts
@@ -1,7 +1,7 @@
 import { PostboyCallbackMessage } from '@artstesh/postboy';
 import { Geometry } from 'ol/geom';
 import { IIdentified } from '../../models/i-identified';
-import { Dictionary } from "@artstesh/collections";
+import { Dictionary } from '@artstesh/collections';
 
 export class GetFeaturesInAreaQuery extends PostboyCallbackMessage<Dictionary<IIdentified[]>> {
   public static readonly ID: string = 'efcf4271-a07a-40e0-9f02-d4dade40b8ac';
