1e4d7ffe80a05776b49d846bc9f91107d4713c15
artstesh <artstesh@mail.ru>
2024-06-18 22:40:18 +0300

    3.1.9

--- projects/maps-components/src/map/messages/events/map-move-end.event.ts
+++ projects/maps-components/src/map/messages/events/map-move-end.event.ts
@@ -1,5 +1,5 @@
 import { PostboyGenericMessage } from '@artstesh/postboy';
-import { MapPosition } from "../../models";
+import { MapPosition } from '../../models';
 
 export class MapMoveEndEvent extends PostboyGenericMessage {
   public static readonly ID: string = 'f21f783e-b5af-4489-9fec-8027fd07a8ad';
