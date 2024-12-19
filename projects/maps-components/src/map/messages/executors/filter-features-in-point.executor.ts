40530abad05c92fde4008b83c90ddd76cd1063c8
artstesh <artstesh@mail.ru>
2024-12-19 11:39:06 +0300

    5.1.16

--- projects/maps-components/src/map/messages/executors/filter-features-in-point.executor.ts
+++ projects/maps-components/src/map/messages/executors/filter-features-in-point.executor.ts
@@ -1,11 +1,11 @@
-import { PostboyExecutor } from "@artstesh/postboy";
-import { Feature } from "ol";
-import { Geometry } from "ol/geom";
+import { PostboyExecutor } from '@artstesh/postboy';
+import { Feature } from 'ol';
+import { Geometry } from 'ol/geom';
 
 /**
  * The FilterFeaturesInPointExecutor class is responsible for filtering features within a given point.
  */
-export class FilterFeaturesInPointExecutor extends PostboyExecutor<Feature<Geometry>[]>{
+export class FilterFeaturesInPointExecutor extends PostboyExecutor<Feature<Geometry>[]> {
   public static readonly ID = '5b859282-5d5a-47ca-a6ad-863a66c8b82b';
   public get id(): string {
     return FilterFeaturesInPointExecutor.ID;
