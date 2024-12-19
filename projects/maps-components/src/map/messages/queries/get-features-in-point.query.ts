21ba8d8c2f7d3973512e966d0cf9144f39776949
artstesh <artstesh@mail.ru>
2024-12-19 11:39:45 +0300

    6.0.7

--- projects/maps-components/src/map/messages/queries/get-features-in-point.query.ts
+++ projects/maps-components/src/map/messages/queries/get-features-in-point.query.ts
@@ -1,11 +1,11 @@
-import { PostboyCallbackMessage, PostboyExecutor } from "@artstesh/postboy";
+import { PostboyCallbackMessage, PostboyExecutor } from '@artstesh/postboy';
 import { Dictionary } from '@artstesh/collections';
 import { IIdentified } from '../../models/i-identified';
 
 /**
  * Represents a query to retrieve features located at a specific geographical point.
  * The query is defined by latitude, longitude, and an optional set of features to ignore.
-*/
+ */
 export class GetFeaturesInPointQuery extends PostboyCallbackMessage<Dictionary<IIdentified[]>> {
   public static readonly ID: string = '7c051c97-bbf1-47eb-baa3-4d1eef6f281f';
 
