d9924efde8ff627af58af51743f59bdb77871102
artstesh <artstesh@mail.ru>
2025-01-30 10:09:14 +0300

    v2

--- projects/maps-components/src/map/messages/executors/filter-features-in-area.executor.ts
+++ projects/maps-components/src/map/messages/executors/filter-features-in-area.executor.ts
@@ -1,6 +1,6 @@
-import { PostboyExecutor } from '@artstesh/postboy';
-import { Feature } from 'ol';
-import { Geometry } from 'ol/geom';
+import { PostboyExecutor } from "@artstesh/postboy";
+import { Feature } from "ol";
+import { Geometry } from "ol/geom";
 
 /**
  * A class that extends the PostboyExecutor to filter geographical features within a specified area.
@@ -11,6 +11,7 @@ import { Geometry } from 'ol/geom';
  * - `features`: An array of geographical features, represented as `Feature<Geometry>` objects, to be filtered by their presence within the specified area.
  */
 export class FilterFeaturesInAreaExecutor extends PostboyExecutor<Feature<Geometry>[]> {
+  static readonly ID = '4d26e1e3-e60f-4e49-804b-5b3eadc2adce'
   constructor(public area: Geometry, public features: Feature<Geometry>[]) {
     super();
   }
