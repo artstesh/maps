0d46eda940016aada6cfb0277d82958d025b7714
artstesh <artstesh@mail.ru>
2025-01-30 10:09:53 +0300

    v3

--- projects/maps-components/src/map/messages/commands/set-map-center.command.ts
+++ projects/maps-components/src/map/messages/commands/set-map-center.command.ts
@@ -1,9 +1,11 @@
-import { PostboyGenericMessage } from '@artstesh/postboy';
+import { PostboyGenericMessage } from "@artstesh/postboy";
 
 /**
  * Forces map to move to a specific point
  */
 export class SetMapCenterCommand extends PostboyGenericMessage {
+  static readonly ID = 'd4e3cf4f-0be7-4c1a-8355-8607d02c3dea'
+
   /**
    *
    * @param lat - the desired latitude
--- projects/maps-components/src/map/services/map-postboy.service.spec.ts
+++ projects/maps-components/src/map/services/map-postboy.service.spec.ts
@@ -2,9 +2,10 @@ import { MapPostboyService } from './map-postboy.service';
 import { Forger } from '@artstesh/forger';
 import { Subject } from 'rxjs';
 import { should } from '@artstesh/it-should';
-import { PostboyCallbackMessage, PostboyGenericMessage } from '@artstesh/postboy';
+import { PostboyCallbackMessage, PostboyGenericMessage } from "@artstesh/postboy";
 
 class TestEvent extends PostboyCallbackMessage<any> {
+  public static readonly ID = '4523b33f-606e-43e2-a6b3-c0e4c05276f4';
   constructor(public value: number) {
     super();
   }
