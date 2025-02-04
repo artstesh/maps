b893aa07172e307e97e5727dfd0e0fa7988f2ce5
artstesh <artstesh@mail.ru>
2025-02-04 13:56:16 +0300

    v6

--- projects/maps-components/src/map/messages/commands/remove-tile.command.ts
+++ projects/maps-components/src/map/messages/commands/remove-tile.command.ts
@@ -3,7 +3,7 @@ import { XYZ } from 'ol/source';
 import { PostboyGenericMessage } from '@artstesh/postboy';
 
 export class RemoveTileCommand extends PostboyGenericMessage {
-  static readonly ID = 'dab0f861-bb25-46da-9388-717bfaae798f';
+  static readonly ID = 'dab0f861-bb25-46da-9388-717bfaae798f'
 
   constructor(public layer: TileLayer<XYZ>) {
     super();
