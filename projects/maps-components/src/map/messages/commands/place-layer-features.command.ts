e706f060441910691debb64cf4fd00f066a6f3ed
artstesh <artstesh@mail.ru>
2025-02-04 14:04:42 +0300

    1.1.16

--- projects/maps-components/src/map/messages/commands/place-layer-features.command.ts
+++ projects/maps-components/src/map/messages/commands/place-layer-features.command.ts
@@ -3,7 +3,7 @@ import { Geometry } from 'ol/geom';
 import { PostboyGenericMessage } from '@artstesh/postboy';
 
 export class PlaceLayerFeaturesCommand extends PostboyGenericMessage {
-  static readonly ID = '5f27a2cd-8d8a-4929-b703-8aee6dcdc21c'
+  static readonly ID = '5f27a2cd-8d8a-4929-b703-8aee6dcdc21c';
 
   constructor(public layer: string, public features: Feature<Geometry>[]) {
     super();
