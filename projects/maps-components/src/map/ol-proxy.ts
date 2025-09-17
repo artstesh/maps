739468b497de3fa1e71efa95e75393ea438859fa
artstesh <artstesh@mail.ru>
2025-09-17 14:53:50 +0300

    Add geometry length calculation support
    Introduce `GetGeometryLengthExecutor` and its handler for calculating geometry lengths using spherical calculations. Register the executor in `MessageRegistratorService`. Update drawing components and related helpers to support additional geometry types (`LineString`). Expand `TextStyleHelper` to include new customization options and enhance tests accordingly.

--- projects/maps-components/src/map/ol-proxy.ts
+++ projects/maps-components/src/map/ol-proxy.ts
@@ -1,3 +1,7 @@
 export {FeatureLike} from 'ol/Feature';
 export {Feature} from 'ol';
 export {Style} from 'ol/style';
+export {Fill, RegularShape, Stroke, Text} from 'ol/style';
+export {StyleLike} from 'ol/style/Style';
+export { Geometry, LineString, Point } from 'ol/geom';
+export * as CircleStyle from 'ol/style/Circle';
