d0c4fcb2019d3cba770264cbe57380fc62dd024b
artstesh <artstesh@mail.ru>
2025-09-22 13:58:27 +0300

    v5

--- projects/maps-components/src/map/messages/commands/modify-feature.command.ts
+++ projects/maps-components/src/map/messages/commands/modify-feature.command.ts
@@ -1,6 +1,6 @@
 import { PostboyCallbackMessage } from '@artstesh/postboy';
 import { FeatureOutputFormat, PolygonModel } from '../../models';
-import Style from 'ol/style/Style';
+import { StyleLike } from 'ol/style/Style';
 
 /**
  * Represents a command to modify a feature with a specific style and output format
@@ -15,13 +15,13 @@ export class ModifyFeatureCommand extends PostboyCallbackMessage<string | null> 
    * Constructs an instance of the class.
    *
    * @param {PolygonModel} model - The model representing the polygon data.
-   * @param {Style} style - The style to be applied to the polygon.
+   * @param {StyleLike} style - The style to be applied to the polygon.
    * @param {FeatureOutputFormat} [format=FeatureOutputFormat.GeoJson] - The output format of the feature, defaults to GeoJson.
    * @param {boolean} [draggable=true] - Determines whether the polygon is draggable, defaults to true.
    */
   constructor(
     public model: PolygonModel,
-    public style: Style,
+    public style: StyleLike,
     public format: FeatureOutputFormat = FeatureOutputFormat.GeoJson,
     public draggable: boolean = true,
   ) {
