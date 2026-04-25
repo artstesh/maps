52e6e798fe9705d38ff52dc5d59b212313a5cb9f
artstesh <artstesh@mail.ru>
2026-04-25 07:37:31 +0300

    5.3.0

--- projects/maps-components/src/map/messages/executors/generate-zoom-control.executor.ts
+++ projects/maps-components/src/map/messages/executors/generate-zoom-control.executor.ts
@@ -1,7 +1,7 @@
 import { PostboyExecutor } from '@artstesh/postboy';
 import { Draw } from 'ol/interaction';
 import { ZoomControlSettings } from '../../map-plate/controls/map-control-zoom/zoom-control.settings';
-import {Zoom} from "ol/control";
+import { Zoom } from 'ol/control';
 
 export class GenerateZoomControlExecutor extends PostboyExecutor<Zoom> {
   static readonly ID = 'c59f4fc3-ccf6-4cb3-a38b-3a8c249114a2';
