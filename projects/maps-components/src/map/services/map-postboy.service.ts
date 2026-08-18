8736697d1a56d31ea76c1bbd8c2663e38bb0bd1a
artstesh <artstesh@mail.ru>
2026-08-18 13:14:37 +0300

    Refactor map services and executors: remove `Math.floor` from zoom calculation, update `CalculateAreaExecutor` to use `number` result, and clean up imports in `MapPostboyService`.

--- projects/maps-components/src/map/services/map-postboy.service.ts
+++ projects/maps-components/src/map/services/map-postboy.service.ts
@@ -1,8 +1,5 @@
-import { Injectable } from '@angular/core';
-import { PostboyService } from '@artstesh/postboy';
-import { StartDrawingCommand } from '../messages/commands/start-drawing.command';
-import { MapClickEvent } from '../messages';
-import { DrawingFinishedEvent } from '../messages/events/drawing-finished.event';
+import {Injectable} from '@angular/core';
+import {PostboyService} from '@artstesh/postboy';
 
 @Injectable()
 export class MapPostboyService extends PostboyService {
